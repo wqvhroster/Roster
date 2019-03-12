5eabdc29a278c125d525bdf6d40ccd25262eaff3
Shan Rixon <shan@Shans-MacBook-Air.local>
2019-03-12 18:18:31 +1100

    Formatting the right sheet now

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 'use strict';
 
-var unavailableStaff = [[],[],[],[],[],[],[]];
-var datesByDaysOfTheWeek = [[], [], [], [],[],[],[]];
+var unavailableStaff = [[], [], [], [], [], [], []];
+var datesByDaysOfTheWeek = [[], [], [], [], [], [], []];
+var pubHols = [];
 
 (function () {
     Office.onReady(function () {
@@ -45,7 +46,7 @@ var datesByDaysOfTheWeek = [[], [], [], [],[],[],[]];
             console.log('nextMonth is: ' + nextMonth);
             monthSelect.value = nextMonth;
             console.log('Selected month is: ' + nextMonth);
-            Excel.run(function (context) {
+            /*Excel.run(function (context) {
                 var wb = context.workbook;
                 var functionResult = wb.functions.year(wb.functions.today());
                 functionResult.load('value');
@@ -57,7 +58,7 @@ var datesByDaysOfTheWeek = [[], [], [], [],[],[],[]];
                         console.log("Debug info: " + JSON.stringify(error.debugInfo));
                     }
                 })
-            });
+            });*/
         });
     });
 
@@ -72,76 +73,80 @@ $('#generate').click(createData);
 function createData() {
     Excel.run(function (context) {
         var currentWorksheet = context.workbook.worksheets.getItemOrNullObject('Staff Data');
-        
-            var staffListRange = currentWorksheet.getUsedRangeOrNullObject();
-            staffListRange.load("values");
+        var staffListRange = currentWorksheet.getUsedRangeOrNullObject();
+        staffListRange.load("values");
+        return context.sync()
+            .then(function () {
 
-            return context.sync()
-                .then(function () {
+                if (staffListRange.values === undefined) {
+                    document.getElementById('errorText').innerHTML = "Error! Was looking for a sheet called Staff Data but couldn't find one.";
+                    return;
+                } else {
+                    document.getElementById('errorText').innerHTML = "Happy days!";
 
-                    if (staffListRange.values === undefined) {
-                        document.getElementById('errorText').innerHTML = "Error! Was looking for a sheet called Staff Data but couldn't find one.";
-                        return;
-                    }  else {
-                        document.getElementById('errorText').innerHTML = "Happy days!";
+                }
+                console.log(JSON.stringify(staffListRange.values, null, 4));
+                console.log(staffListRange.values.length);
+                var rosterTableHeaderRow = [];
 
-                    }
-                    console.log(JSON.stringify(staffListRange.values, null, 4));
-                    console.log(staffListRange.values.length);
-                    var rosterTableHeaderRow = [];
+                // Start at line 1 to ignore the header row
+                for (var i = 1; i < staffListRange.values.length; i++) {
 
-                    // Start at line 1 to ignore the header row
-                    for (var i = 1; i < staffListRange.values.length; i++) {
-
-                            if (staffListRange.values[i][0] != '') {
-                                rosterTableHeaderRow.push(staffListRange.values[i][0]);
-                                //rosterTable.getHeaderRowRange().values[i] = rosterTableHeaderRow;
-                            }
-
-                            for (var j = 1; j < staffListRange.values[i].length; j++) {
-                                if (staffListRange.values[i][j] != "") {
-                                    unavailableStaff[j-1].push(i-1);
-                                }
-                            }
-                        
+                    if (staffListRange.values[i][0] != '') {
+                        rosterTableHeaderRow.push(staffListRange.values[i][0]);
                     }
 
-                    var calendar = generateCalendar(rosterTableHeaderRow.length);
-                    rosterTableHeaderRow.unshift('', '');// Two columns for the day of the week and the date
-                    console.log(rosterTableHeaderRow);
+                    for (var j = 1; j < staffListRange.values[i].length; j++) {
+                        if (staffListRange.values[i][j] != "") {
+                            unavailableStaff[j - 1].push(i - 1);
+                        }
+                    }
 
-                    calendar.unshift(rosterTableHeaderRow);
-                    console.log(calendar);
+                }
 
-                    var lastCell = convertToNumberingScheme(rosterTableHeaderRow.length);
-                    lastCell += calendar.length;
-                    //console.log("the last cell for the roster range is: " + lastCell);
+                var calendar = generateCalendar(rosterTableHeaderRow.length);
+                rosterTableHeaderRow.unshift('', '');// Two columns for the day of the week and the date
+                console.log(rosterTableHeaderRow);
 
-                    context.workbook.worksheets.add("Roster").activate();
-                    var rosterRange = context.workbook.worksheets.getActiveWorksheet().getRange("A1:" + lastCell);
-                    rosterRange.values = calendar;
-                    rosterRange.format.autofitColumns();
+                calendar.unshift(rosterTableHeaderRow); // Add the header row to the top of the calendar
+                console.log("Here's the calendar: " + calendar);
+                // Figure out what the public holidays are:
+                //var pubHolSheet = context.workbook.worksheets.getItemOrNullObject('Public Holidays');
+                //var pubHolRange = pubHolSheet.getUsedRangeOrNullObject();
+                //populatePublicHolidayData(context, pubHolRange);
+
+                // Create the roster
+                var lastCell = convertToNumberingScheme(rosterTableHeaderRow.length);
+                lastCell += calendar.length;
+                console.log("the last cell for the roster range is: " + lastCell);
+                context.workbook.worksheets.add("Roster");
+                var rosterRange = context.workbook.worksheets.getItemOrNullObject('Roster').getRange("A1:" + lastCell);
+                rosterRange.values = calendar;
+
+//                return context.sync().then(function(context) {
                     formatRoster(context, rosterRange);
 
-                    return context.sync();
+//                });
 
-                });        
-        
-        
+                return context.sync();
 
-        
+            });
+
+
+
+
     }
-        ).catch(function (error) {
-            console.log("Error: " + error);
-            if (error instanceof OfficeExtension.Error) {
-                console.log("Debug info: " + JSON.stringify(error.debugInfo));
-            }
-        });
+    ).catch(function (error) {
+        console.log("Error: " + error);
+        if (error instanceof OfficeExtension.Error) {
+            console.log("Debug info: " + JSON.stringify(error.debugInfo));
+        }
+    });
 }
 
 function formatRoster(context, rosterRange) {
     rosterRange.load("values");
-    return context.sync().then(function() {
+    return context.sync().then(function () {
         //console.log("range values size is: " + rosterRange.values.length);
         for (var i = 0; i < rosterRange.values.length; i++) {
             // Find the weekends and set the fill to grey
@@ -149,9 +154,9 @@ function formatRoster(context, rosterRange) {
             //console.log ("Current row is: " + JSON.stringify(row, null, 4));
             //console.log ("First column in the current row is: " + row[0]);
             if (row[0] === "Sat" || row[0] === "Sun") {
-                var rangeString = "A" + (i+1) + ":" + convertToNumberingScheme(row.length) + (i+1);
-                //console.log(rangeString);
-                var weekendRange = context.workbook.worksheets.getActiveWorksheet().getRange(rangeString);
+                var rangeString = "A" + (i + 1) + ":" + convertToNumberingScheme(row.length) + (i + 1);
+                console.log(rangeString);
+                var weekendRange = context.workbook.worksheets.getItemOrNullObject('Roster').getRange(rangeString);
                 weekendRange.format.fill.color = "#C8C8C8";
             }
 
@@ -162,15 +167,16 @@ function formatRoster(context, rosterRange) {
             //console.log("For dates: " + datesByDaysOfTheWeek[i] + ", these staff are unavailable: " + unavailableStaff[i]);
             for (var j = 0; j < staff.length; j++) {
                 for (var k = 0; k < days.length; k++) {
-                    var column = staff[j] +3;
+                    var column = staff[j] + 3;
                     var row = days[k] + 1;
                     //console.log("I wish to grey out cell: " + " j: " + column + ", k: " + row);
                     var rangeString = convertToNumberingScheme(column) + row;
-                    var dayOffRange = context.workbook.worksheets.getActiveWorksheet().getRange(rangeString);
+                    var dayOffRange = context.workbook.worksheets.getItemOrNullObject('Roster').getRange(rangeString);
                     dayOffRange.format.fill.color = "#C8C8C8";
                 }
             }
         }
+        rosterRange.format.autofitColumns();
         return context.sync();
 
     });
@@ -183,7 +189,7 @@ function generateCalendar(numColumns) {
     var startDate = new Date(yearSelect.value, monthSelect.selectedIndex, 1);
     var endDate = new Date(yearSelect.value, monthSelect.selectedIndex + 1, 0);
     var endDay = endDate.getDate();
-    console.log ("Start date is: " + startDate.toDateString() + "; end date is: " + endDate.toDateString());
+    console.log("Start date is: " + startDate.toDateString() + "; end date is: " + endDate.toDateString());
     var calendar = [];
     for (var i = 1; i < endDay + 1; i++) {
         var row = [];
@@ -195,21 +201,32 @@ function generateCalendar(numColumns) {
         for (var j = 0; j < numColumns; j++) {
             row.push(""); //Everyone starts off working every day
         }
-        calendar[i-1] = row;
+        calendar[i - 1] = row;
     }
-    //console.log(calendar);
     return calendar;
+}
+
+function populatePublicHolidayData(context, pubHolRange) {
+    pubHolRange.load("values");
+    return context.sync().then(function() {
+        if (pubHolRange.values === undefined) {
+            document.getElementById('errorText').innerHTML = "Warning! Was looking for a sheet called Public Holidays but couldn't find one. Public holidays will not be applied! Continuing anyway...";
+        } 
+    
+    })
+    
+
 }
 
 function convertToNumberingScheme(number) {
     var baseChar = ("A").charCodeAt(0),
-        letters  = "";
-  
+        letters = "";
+
     do {
-      number -= 1;
-      letters = String.fromCharCode(baseChar + (number % 26)) + letters;
-      number = (number / 26) >> 0; // quick `floor`
-    } while(number > 0);
-  
+        number -= 1;
+        letters = String.fromCharCode(baseChar + (number % 26)) + letters;
+        number = (number / 26) >> 0; // quick `floor`
+    } while (number > 0);
+
     return letters;
-  }
+}
